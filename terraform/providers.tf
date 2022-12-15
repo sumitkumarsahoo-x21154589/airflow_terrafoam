aea71bc92c9dab66abdd0418ed7f611b55b804ea
SumitKumarSahoo <x21154589@student.ncirl.ie>
2022-12-15 09:30:12 +0000

    accesskey changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  access_key = "AKIAXLJZTDDBNRIGCZXM"
-  secret_key = "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"
+  access_key = "AKIAXLJZTDDBKIQG5FEC"
+  secret_key = "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"
 }
