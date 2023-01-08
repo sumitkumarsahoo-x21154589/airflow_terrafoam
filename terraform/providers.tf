f90e801d7c4d1163e1a47709f76ee9aae5513b19
SumitKumarSahoo <x21154589@student.ncirl.ie>
2023-01-08 13:02:11 +0000

    removed keys

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,14 +2,13 @@ terraform {
   required_version = ">= 0.12"
 }
 
-output "log" {
-  value = "${yamldecode(file("secretkey.yaml"))}"
-  access_key = value["access_key"]
-  secret_key =  value["secret_key"]
-}
+# output "log" {
+#   value = "${yamldecode(file("secretkey.yaml"))}"
+#   access_key = value["access_key"]
+#   secret_key =  value["secret_key"]
+# }
 
 provider "aws" {
   region = var.aws_region
-  access_key = log.access_key #removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
-  secret_key = log.secret_key #removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
+
 }
