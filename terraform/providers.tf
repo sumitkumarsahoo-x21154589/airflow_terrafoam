ac96f711d2b75e1413c9c669eaa8162ee57e70da
SumitKumarSahoo <x21154589@student.ncirl.ie>
2023-01-08 11:29:13 +0000

    added yml accessky

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,8 +2,14 @@ terraform {
   required_version = ">= 0.12"
 }
 
+output "log" {
+  value = "${yamldecode(file("secretkey.yaml"))}"
+  access_key = value["access_key"]
+  secret_key =  value["secret_key"]
+}
+
 provider "aws" {
   region = var.aws_region
-  access_key = ""#removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
-  secret_key = ""#removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
+  access_key = log.access_key #removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
+  secret_key = log.secret_key #removed accesskey , due to security reason , if needed kindly contact x21154589@student.ncirl.ie
 }
--- terraform/s3_buckets.tf
+++ terraform/s3_buckets.tf
@@ -1,20 +1,20 @@
 
-### passing list of buckets
+# ### passing list of buckets
 
-variable "s3_folders" {
-  type        = list(string)
-  description = "The list of S3 folders to create"
-  default     = ["x21154589_input", "x21154589_output","x21154589_scripts"]
-}
+# variable "s3_folders" {
+#   type        = list(string)
+#   description = "The list of S3 folders to create"
+#   default     = ["x21154589_input", "x21154589_output","x21154589_scripts"]
+# }
 
-resource "aws_s3_bucket" "x21154589_bucket_v2" {
-  bucket = "emrbuckettestsumit"
-}
+# resource "aws_s3_bucket" "x21154589_bucket_v2" {
+#   bucket = "emrbuckettestsumitv2"
+# }
 
-resource "aws_s3_bucket_object" "folders" {
-    count   = "${length(var.s3_folders)}"
-    bucket = aws_s3_bucket.x21154589_bucket_v2.id
-    acl    = "private"
-    key    = "${var.s3_folders[count.index]}/"
-    source = "/dev/null"
-}
+# resource "aws_s3_bucket_object" "folders" {
+#     count   = "${length(var.s3_folders)}"
+#     bucket = aws_s3_bucket.x21154589_bucket_v2.id
+#     acl    = "private"
+#     key    = "${var.s3_folders[count.index]}/"
+#     source = "/dev/null"
+# }
